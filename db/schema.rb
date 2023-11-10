9e20a35a17b4939f02fc684e871cbd5db473f785
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-11-09 23:39:17 -0300

    Reset db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_173459) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_08_235140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lectures", force: :cascade do |t|
     t.string "name"
@@ -22,23 +29,58 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_173459) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "on_cart_products", force: :cascade do |t|
+    t.string "status", default: "pending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "reservation_id", default: 0, null: false
+    t.bigint "user_id", default: 0, null: false
+    t.index ["reservation_id"], name: "index_on_cart_products_on_reservation_id"
+    t.index ["user_id"], name: "index_on_cart_products_on_user_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "color"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.string "description"
+    t.string "category"
+    t.integer "year"
+    t.boolean "is_presice_year", default: true
+    t.decimal "price"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "removed", default: false
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.date "date"
-    t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.bigint "lecture_id", null: false
+    t.bigint "product_id", null: false
     t.boolean "removed", default: false
-    t.index ["lecture_id"], name: "index_reservations_on_lecture_id"
+    t.boolean "paid", default: false
+    t.boolean "delivered", default: false
+    t.index ["product_id"], name: "index_reservations_on_product_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "shipping_address"
+    t.integer "phone_number1"
+    t.integer "phone_number2"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -50,6 +92,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_173459) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reservations", "lectures"
+  add_foreign_key "on_cart_products", "reservations"
+  add_foreign_key "on_cart_products", "users"
+  add_foreign_key "reservations", "products"
   add_foreign_key "reservations", "users"
 end
